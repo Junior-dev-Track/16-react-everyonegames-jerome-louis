dc5253370af5a60cc0772ab62ca5d812f712a99a
Miqi007 <louis.pavier@live.fr>
2024-04-24 16:53:02 +0200

    initial link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // import { useState, useEffect } from 'react'
+import process from "../.eslintrc.cjs"
 
 import './App.css'
 
 function App() {
   
+  fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_API_KEY}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(err => console.error("Erreur lors de la requête à l'API RAWG:", err));
+
 
   return (
     <>
