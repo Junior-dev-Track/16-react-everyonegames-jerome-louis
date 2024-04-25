e444136c969c09e88cf195e8b728edd5b688f9be
Miqi007 <louis.pavier@live.fr>
2024-04-25 13:52:43 +0200

    Img optimization

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,8 @@
 const Logo = () => {
     return (
         <div className='logo'>
-            <img src="src/assets/logo/logo_JeLo.png" alt="website logo" style={{ width: '10%' }} />
+            <img src="src/assets/logo/logo_JeLo.png" alt="website logo" style={{ width: '8%', borderRadius: '50%' }} />
+
 
         </div>
     );
