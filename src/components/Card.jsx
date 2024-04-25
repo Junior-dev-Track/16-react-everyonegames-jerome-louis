95bf857d8f1f34f5a6ec86cc29cd415e7fc803b9
Miqi007 <louis.pavier@live.fr>
2024-04-25 15:04:03 +0200

    Infinite scroll but duplicates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
-
-
+/* eslint-disable react/prop-types */
 const Card = ({game}) => {
     return (
         <li className="card">
-            <img src="{game.background_image.}" alt="Game image" />
+            <img src={game.background_image} alt="Game image" style={{width:'10%'}} />
             <div className="infos">
-                <h2>Game name</h2>
+                <h2>{game.name}</h2>
             </div>
         </li>
     );
--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -5,13 +5,35 @@ import { v4 as uuid } from "uuid";
 const Games = () => {
     // state
     const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
 
     // effect
     useEffect(() => {
-        fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}`)
+        fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=${page}`)
         .then(response => response.json())
-        .then(data => setData(data.results))
+        .then(data => {
+            setData(prevData => [...prevData, ...data.results]);
+            setTotalPages(data.count / 20); // Assuming 20 items per page
+        })
         .catch(error => console.error('Error fetching data:', error));
+    }, [page]);
+
+    // Scroll event handler
+    const handleScroll = () => {
+        const threshold = 200; // Distance from the bottom of the page to trigger the fetch
+        const isNearBottom = window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - threshold;
+        if (isNearBottom) {
+            setPage(prevPage => prevPage + 1);
+        }
+    };
+    
+    
+    
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
@@ -19,7 +41,7 @@ const Games = () => {
             <h1>Games</h1>
             <ul>
                 {data.map((game) => (
-                    <Card key={uuid()} gameName={game}/>
+                    <Card key={uuid()} game={game}/>
                 ))}
             </ul>
         </div>
