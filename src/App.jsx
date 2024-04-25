f3519976dc422b58828ad7fdd15f71540a59dbf3
Miqi007 <louis.pavier@live.fr>
2024-04-25 09:43:20 +0200

    Initial config

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 // import { useState, useEffect } from 'react'
-import process from "../.eslintrc.cjs"
-
-import './App.css'
+import './styles/App.css'
 
 function App() {
   
-  fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_API_KEY}`)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => console.error("Erreur lors de la requête à l'API RAWG:", err));
-
-
   return (
     <>
       <h1>Everyone games</h1>
