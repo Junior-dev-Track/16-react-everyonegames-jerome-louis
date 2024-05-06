536f12f54e00058c8bc7ac6aab4f234ed5e98059
Miqi007 <louis.pavier@live.fr>
2024-05-06 15:02:30 +0200

    Logo platform ok

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,14 @@
 /* eslint-disable react/prop-types */
+import { v4 as uuid } from "uuid";
+import { FaWindows } from "react-icons/fa";
+import { FaPlaystation } from "react-icons/fa";
+import { FaXbox } from "react-icons/fa";
+import { FaAppStoreIos } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
+import { FaLinux } from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { IoLogoAndroid } from "react-icons/io";
+
 const Card = ({ game, onClick }) => {
     return (
         <div className="cardWrapper" onClick={onClick}>
@@ -6,6 +16,21 @@ const Card = ({ game, onClick }) => {
                 <img src={game.background_image} alt="Game image" />
                 <div className="infos">
                     <h2>{game.name}</h2>
+                    <ul>
+                        {game.parent_platforms.slice(0, 3).map((platformObj) => (
+                            <li key={uuid()}>
+                                {platformObj.platform.id === 1 && <FaWindows /> }
+                                {platformObj.platform.id === 2 && <FaPlaystation /> }
+                                {platformObj.platform.id === 3 && <FaXbox /> }
+                                {platformObj.platform.id === 4 && <FaAppStoreIos /> }
+                                {platformObj.platform.id === 5 && <FaApple /> }
+                                {platformObj.platform.id === 6 && <FaLinux /> }
+                                {platformObj.platform.id === 7 && <SiNintendo /> }
+                                {platformObj.platform.id === 8 && <IoLogoAndroid /> }
+
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </li>
         </div>
@@ -13,3 +38,20 @@ const Card = ({ game, onClick }) => {
 };
 
 export default Card;
+
+/*
+    return (
+    <div>
+      <h1>{game.name}</h1>
+      <div>
+        {game.platforms.map((platform) => (
+          <div key={platform.id}>
+            {platform.id === 1 && <FaWindows />}
+            <span>{platform.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+*/
