3584202a7fd2c5a2c769c4059c27ee3828ee3afe
Miqi007 <louis.pavier@live.fr>
2024-05-07 16:32:45 +0200

    Game detail ok

--- src/components/AboutGame.jsx
+++ src/components/AboutGame.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
+import { FaRegClock } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
+import { TbTargetArrow } from "react-icons/tb";
 
 const AboutGame = ({ gameId }) => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({});
+    const [img, setImg] = useState([]);
 
     useEffect(() => {
         fetch(`https://api.rawg.io/api/games/${gameId}?key=${import.meta.env.VITE_API_KEY}`)
@@ -10,15 +15,33 @@ const AboutGame = ({ gameId }) => {
        .then(data => {
             setData(data);
         })
-       .catch(error => console.error('Error fetching data:', error));
+       .catch(error => console.error('Error fetching game data:', error));
     }, [gameId]);
 
+    useEffect(() => {
+        fetch(`https://api.rawg.io/api/games/${gameId}/screenshots?key=${import.meta.env.VITE_API_KEY}`)
+       .then(response => response.json())
+       .then(img => {
+            setImg(img.results);
+        })
+       .catch(error => console.error('Error fetching screenshots data:', error));
+    }, [gameId]);
 
     return (
         <div className="games">
             <div className="text">
-            <h2>{data.name}</h2>
-            <p></p>
+                <h2>{data.name}</h2>
+                <p>Average playtime: {data.playtime}h <FaRegClock/></p>
+                <p>{data.rating} <FaStar color="orange" fontSize="1em" /></p>
+                <p>Number of reviews: {data.reviews_count} <IoPersonSharp /></p>
+                <p>Metacritic score: {data.metacritic} <TbTargetArrow /></p>
+            </div>
+            <div className="images">
+                <ul>
+                {img.map((image) => {
+                    return <li key={image.id}><img src={image.image} alt="" /></li>;
+                })}
+                </ul>
             </div>
         </div>
     );
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,7 @@
-
-
 const Logo = () => {
     return (
         <div className='logo'>
-            <img src="src/assets/logo/logo_JeLo.png" alt="website logo"/>
+            <img src="/src/assets/logo/logo_JeLo.png" alt="website logo"/>
         </div>
     );
 };
