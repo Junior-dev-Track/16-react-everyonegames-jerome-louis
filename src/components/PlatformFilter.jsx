9eac13598539c12b22cc6fedb9921db6093ea473
jeromeguisse <jeromeguisse@gmail.com>
2024-05-07 16:54:42 +0200

    Bouton filtrant fonctionnel et terminé

--- src/components/PlatformFilter.jsx
+++ src/components/PlatformFilter.jsx
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';  // Import useNavigate from react-router-dom
+import { useNavigate } from 'react-router-dom';
 
 const PlatformFilter = () => {
-    const [platforms, setPlatforms] = useState([]); // State to store platforms
-    const navigate = useNavigate(); // useNavigate hook for navigation
-
-    const handleFilterChange = (event) => {
-        const selectedId = event.target.value;
-        console.log(`Navigating to new platform with ID: ${selectedId}`);  // Log the navigation action
-        navigate(`/?platforms=${encodeURIComponent(selectedId)}`); // Navigate to update the URL
-    };
+    const [platforms, setPlatforms] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const API_KEY = import.meta.env.VITE_API_KEY; // Ensure your API key is prefixed with VITE_
+        const API_KEY = import.meta.env.VITE_API_KEY;
         const fetchPlatforms = async () => {
             try {
                 const response = await axios.get(`https://api.rawg.io/api/platforms?key=${API_KEY}`);
-                setPlatforms(response.data.results); // Assuming 'results' contains the list of platforms
-                console.log(response.data.results); // Log to see what the API returned
+                setPlatforms(response.data.results);
+                console.log(response.data.results);
             } catch (error) {
                 console.error('Error fetching platforms:', error);
             }
@@ -27,8 +21,15 @@ const PlatformFilter = () => {
         fetchPlatforms();
     }, []);
 
+    const handleFilterChange = (event) => {
+        const selectedId = event.target.value;
+        console.log(`Navigating to new platform with ID: ${selectedId}`);
+        navigate(`/?platforms=${encodeURIComponent(selectedId)}`);
+    };
+
     return (
-        <select onChange={handleFilterChange}>  // Make sure onChange is set up correctly
+        <select className="filter-button" onChange={handleFilterChange}>
+            <option disabled selected>Filter by platform</option>
             {platforms.length > 0 ? (
                 platforms.map((platform) => (
                     <option key={platform.id} value={platform.id}>
